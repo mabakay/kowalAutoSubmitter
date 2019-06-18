82be026c3c40dba81297f19398d06ee70d94a5a7
Mateusz Bogusz <mabakay@o2.pl>
2019-06-18 12:03:54 +0200

    Added sound functionality on product scan

--- kowalAutoSubmitter.user.js
+++ kowalAutoSubmitter.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Kowal Auto Submiter
 // @namespace    http://www.google.com/search?q=mabakay
-// @version      1.1
+// @version      1.2
 // @description  Allows to automaticaly parse and submit of scaned codes.
 // @description:pl-PL Pozwala na automatyczne parsowanie i wysyłanie zeskanowanych kodów.
 // @author       mabakay
 // @copyright    2019, mabakay
-// @date         16 may 2019
+// @date         18 June 2019
 // @license      GPL-3.0
 // @run-at       document-end
 // @supportURL   https://github.com/mabakay/kowalAutoSubmitter
@@ -52,7 +52,7 @@
     // Resize whole data table
     tableBlock.setAttribute('style', 'width: 100%');
 
-    // inject extended fields
+    // Inject extended fields
     formBlock.prepend(htmlToElement('<div class="ui-panel-content ui-widget-content"><div><div class="row"><div class="hidden-md col-xs-4"><label for="ex:kowal_paste">Kod produktu (<input id="ex:kowal_autosubmit" type="checkbox" checked="checked"><label for="ex:kowal_autosubmit">wysyłaj automatycznie</label>)</label></div><div class="hidden-md col-xs-8"><input id="ex:kowal_paste" type="text" placeholder="Zeskanuj lub przepisz kod..." class="ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all"></div></div></div></div>'));
     formBlock.prepend(htmlToElement('<div class="ui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all"><span class="ui-panel-title">Automatyzacja wsadu</span></div>'));
 
@@ -101,6 +101,71 @@
         }
     });
 
+    // Detect result table change
+    var resetButtonClicked = false;
+
+    var resetButton = document.getElementById('serialnumberForm:resetDataTable');
+    if (resetButton != null) {
+        resetButton.addEventListener('click', function () {
+            resetButtonClicked = true;
+        });
+    }
+
+    var resultTable = document.getElementById('serialnumberForm:snListPanel');
+
+    var callback = function (mutations, observer) {
+        if (resetButtonClicked) {
+            resetButtonClicked = false;
+            return;
+        }
+
+        for (var m = 0; m < mutations.length; ++m) {
+            if (mutations[m].type == 'childList' && mutations[m].addedNodes.length > 0) {
+                for (var i = 0; i < mutations[m].addedNodes.length; ++i) {
+                    if (mutations[m].addedNodes[i].id == "serialnumberForm:dataTable") {
+                        var table = mutations[m].addedNodes[i].getElementsByTagName('table')[0];
+
+                        if (table.rows.length > 0) {
+                            var lastRow = table.rows[table.rows.length - 1];
+
+                            if (table.rows.length == 2 && lastRow.getElementsByTagName('td').length == 1) {
+                                return;
+                            }
+
+                            // Check if last row has failed status
+                            if (lastRow.getElementsByClassName('snRowStyleFailed').length > 0) {
+                                console.log('fail');
+                                playSound('fail');
+                            } else {
+                                console.log('success');
+                                playSound('success');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    var observer = new MutationObserver(callback);
+    observer.observe(resultTable, { attributes: false, childList: true, subtree: false });
+
+    // Sound functionality
+    var sounds = {
+        "fail": {
+            url: "https://www.bogusz.it/sounds/sound.php?s=fail"
+        },
+        "success": {
+            url: "https://www.bogusz.it/sounds/sound.php?s=success"
+        }
+    };
+
+    var soundContext = new AudioContext();
+
+    for (var key in sounds) {
+        loadSound(key);
+    }
+
     // Methods
     function fail() {
         alert('Struktura strony uległa zmianie. Brak możliwości aktywowania rozszerzenia.');
@@ -145,5 +210,49 @@
         }
 
         return false;
+    }
+
+    function loadSound(name) {
+        var sound = sounds[name];
+
+        var url = sound.url;
+        var buffer = sound.buffer;
+
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+
+        request.onload = function () {
+            soundContext.decodeAudioData(request.response, function (newBuffer) {
+                sound.buffer = newBuffer;
+            });
+        }
+
+        request.send();
+    }
+
+    function playSound(name, options) {
+        var sound = sounds[name];
+        var soundVolume = sounds[name].volume || 1;
+
+        var buffer = sound.buffer;
+        if (buffer) {
+            var source = soundContext.createBufferSource();
+            source.buffer = buffer;
+
+            var volume = soundContext.createGain();
+
+            if (options) {
+                if (options.volume) {
+                    volume.gain.value = soundVolume * options.volume;
+                }
+            } else {
+                volume.gain.value = soundVolume;
+            }
+
+            volume.connect(soundContext.destination);
+            source.connect(volume);
+            source.start(0);
+        }
     }
 })();
